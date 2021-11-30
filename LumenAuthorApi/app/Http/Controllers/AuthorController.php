e2397f028f1d48aeacd65993cdff8830cf55cfe7
duvg <duviel7@gmail.com>
2021-11-30 11:45:54 -0500

    First commit, author api service, congifure model and migration

---
+++ LumenAuthorApi/app/Http/Controllers/AuthorController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ExampleController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Return authors list
+     * @return Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+    }
+
+    /**
+     * Create an instance of author
+     * @return Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+    }
+
+    /**
+     * Create an specific author
+     * @return Illuminate\Http\Response
+     */
+    public function show(Request $request)
+    {
+        
+    }
+
+    /**
+     * Update an author
+     * @return Illuminate\Http\Response
+     */
+    public function update(Request $request, $author)
+    {
+        
+    }
+
+
+    /**
+     * Remoeve an existing author
+     * @return Illuminate\Http\Response
+     */
+    public function destroy($author)
+    {
+        
+    }
+}
---
+++ LumenAuthorApi/app/Models/Author.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Author extends Model 
+{
+    
+    use HasFactory;
+        
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'gender',
+        'counter',
+    ];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+    ];
+}
